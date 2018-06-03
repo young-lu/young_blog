d89ed80a93be0875b65c8ca4fe7c5066b3a41fc9
Young Lu <connorylu@gmail.com>
2018-06-03 16:59:46 -0400

    finished djangogirls; need to make my own changes

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from datetime import datetime
+from .models import Post
+from .forms import PostForm
 
 # Create your views here.
+def post_list(request) :
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	date = datetime.now()
+	return render(request, 'blog/post_list.html', {'posts': posts, 'date': date})
+
+def post_detail(request, pk) :
+	post = get_object_or_404(Post, pk=pk)
+	date = datetime.now()
+	return render(request, 'blog/post_detail.html', {'post': post, 'date': date})
+
+def post_new(request) :
+	# form = PostForm()
+	# date = datetime.now()
+	# return render(request, 'blog/post_edit.html', {'form': form, 'date': date})
+
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+		    post = form.save(commit=False)
+		    post.author = request.user
+		    post.published_date = timezone.now()
+		    post.save()
+		    return redirect('post_detail', pk=post.pk)
+
+	else:
+		form = PostForm()
+	return render(request, 'blog/post_edit.html', {'form': form})
